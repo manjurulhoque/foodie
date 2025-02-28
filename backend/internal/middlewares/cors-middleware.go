59719369fc419fe9c076f26e14ac7c742b07f05d
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-03-01 01:05:11 +0600

    update CORS

--- backend/internal/middlewares/cors-middleware.go
+++ backend/internal/middlewares/cors-middleware.go
@@ -8,7 +8,12 @@ import (
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
 		origin := c.Request.Header.Get("Origin")
-        c.Writer.Header().Set("Access-Control-Allow-Origin", origin) // security risk
+		referer := c.Request.Header.Get("Referer")
+
+		if origin == "" && referer != "" {
+			origin = referer
+		}
+		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
